50b962081c354377720f765102e802ee9640c3e7
Yulya Davydova <saewelo@gmail.com>
2023-04-19 01:02:37 +0200

    Added Task 4 (MultiThreading.Task4.Threads.Join.csproj)

--- MultiThreading.Task4.Threads.Join/Program.cs
+++ MultiThreading.Task4.Threads.Join/Program.cs
@@ -10,11 +10,14 @@
  */
 
 using System;
+using System.Threading;
 
 namespace MultiThreading.Task4.Threads.Join
 {
     class Program
     {
+        static Semaphore semaphore = new Semaphore(0, 10);
+
         static void Main(string[] args)
         {
             Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
@@ -26,9 +29,43 @@ namespace MultiThreading.Task4.Threads.Join
 
             Console.WriteLine();
 
-            // feel free to add your code
+            int state = 10;
+
+            RecursiveThread(state);
+
+            semaphore.Release();
+            RecursiveThreadPool(state);
+
 
             Console.ReadLine();
+        }
+
+        static void RecursiveThread(int state)
+        {
+            Console.WriteLine($"Recursive Thread {Thread.CurrentThread.ManagedThreadId} with state {state}");
+
+            if (state > 0)
+            {
+                Thread thread = new Thread(() => RecursiveThread(state - 1));
+                thread.Start();
+                thread.Join();
+            }
+        }
+
+        static void RecursiveThreadPool(int state)
+        {
+            
+
+            Console.WriteLine($"Recursive Thread Pool {Thread.CurrentThread.ManagedThreadId} with state {state}");
+
+            if (state > 0)
+            {
+                ThreadPool.QueueUserWorkItem(_ => RecursiveThreadPool(state - 1));
+            }
+
+            semaphore.Release();
+
+            semaphore.WaitOne();
         }
     }
 }
