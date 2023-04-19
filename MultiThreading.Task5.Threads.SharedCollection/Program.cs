a7b8cdfd857e89badf57b2588d6ea2953377d005
Yulya Davydova <saewelo@gmail.com>
2023-04-19 21:50:35 +0200

    Added Task 5 (MultiThreading.Task5.Threads.SharedCollection.csproj)

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,17 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        static object lockObj = new object();
+        static List<int> sharedList = new List<int>();
+        static bool addFinished = false;
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -17,9 +23,53 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
+            Thread addThread = new Thread(AddItems);
+            Thread printThread = new Thread(PrintItems);
+
+            addThread.Start();
+            printThread.Start();
+
+            addThread.Join();
+            printThread.Join();
 
             Console.ReadLine();
+        }
+
+        static void AddItems()
+        {
+            lock (lockObj)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    sharedList.Add(i);
+                    Console.WriteLine($"Item {i} added to collection.");
+                    Monitor.Pulse(lockObj);
+                    if (i != 10)
+                        Monitor.Wait(lockObj);
+                }
+                addFinished = true;
+                Monitor.Pulse(lockObj);
+            }
+        }
+
+        static void PrintItems()
+        {
+            lock (lockObj)
+            {
+                while (!addFinished || sharedList.Count > 0)
+                {
+                    Monitor.Wait(lockObj);
+                    Console.Write("[");
+                    for (int i = 0; i < sharedList.Count; i++)
+                    {
+                        Console.Write(sharedList[i]);
+                        if (i < sharedList.Count - 1)
+                            Console.Write(", ");
+                    }
+                    Console.WriteLine("]");
+                    Monitor.Pulse(lockObj);
+                }
+            }
         }
     }
 }
